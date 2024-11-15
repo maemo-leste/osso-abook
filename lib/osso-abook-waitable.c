3f055bc47b5940819b9cf9be4a171174f08751da
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2024-11-15 09:51:57 +0200

    lib/osso-abook-waitable: document
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- lib/osso-abook-waitable.c
+++ lib/osso-abook-waitable.c
@@ -1,3 +1,38 @@
+/*
+ * osso-abook-waitable.c
+ *
+ * Copyright (C) 2020 Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
+ *
+ * This library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * SECTION:osso-abook-waitable
+ * @short_description: Generic interfaces for object with asynchronous
+ * initialization.
+ *
+ * This interface provides a generic method for object with asynchronous
+ * initialization to inform their clients that they finished initialization.
+ *
+ * In order to implement #OssoABookWaitable, derived classes should implement a
+ * stack of opaque closures #OssoABookWaitableClosure and the virtual
+ * `push()` and `pop()` methods for adding or removing closures from the stack.
+ * Inherited classes should indicate that they have become ready by calling
+ * osso_abook_waitable_notify().
+ *
+ */
+
 #include "config.h"
 
 #include <gdk/gdk.h>
@@ -38,6 +73,14 @@ osso_abook_waitable_destroy_closure(OssoABookWaitableClosure *closure)
   g_slice_free(OssoABookWaitableClosure, closure);
 }
 
+/**
+ * osso_abook_waitable_cancel:
+ * @waitable: a #OssoABookWaitable
+ * @closure: a return value from osso_abook_waitable_call_when_ready()
+ *
+ * Returns: %TRUE if cancel was successful, else %FALSE (for example if @closure
+ * was already called or cancelled)
+ */
 gboolean
 osso_abook_waitable_cancel(OssoABookWaitable *waitable,
                            OssoABookWaitableClosure *closure)
@@ -59,6 +102,22 @@ osso_abook_waitable_cancel(OssoABookWaitable *waitable,
   return TRUE;
 }
 
+/**
+ * osso_abook_waitable_call_when_ready:
+ * @waitable: a #OssoABookWaitable
+ * @callback: a function to call when ready
+ * @user_data: additional data to pass to @callback
+ * @destroy: an optional function to free @user_data when no longer needed
+ *
+ * Registers the specified @callback to be executed when @waitable becomes
+ * ready. The meaning of 'ready' is dependant on the concrete implementation
+ * classes, but once a #OssoABookWaitable object becomes ready it does not
+ * become 'unready' again. If @waitable is already ready when this function is
+ * called, @callback will be executed immediately.
+ *
+ * Returns: an opaque closure that can be used later to cancel this
+ * registration by calling osso_abook_waitable_cancel().
+ */
 OssoABookWaitableClosure *
 osso_abook_waitable_call_when_ready(OssoABookWaitable *waitable,
                                     OssoABookWaitableCallback callback,
@@ -93,6 +152,15 @@ osso_abook_waitable_call_when_ready(OssoABookWaitable *waitable,
   return closure;
 }
 
+/**
+ * osso_abook_waitable_is_ready:
+ * @waitable: a #OssoABookWaitable
+ * @error: a return location for error reporting
+ *
+ * Checks whether @waitable is ready or not.
+ *
+ * Returns: %TRUE if @waitable is ready, else %FALSE
+ */
 gboolean
 osso_abook_waitable_is_ready(OssoABookWaitable *waitable, GError **error)
 {
@@ -137,6 +205,12 @@ osso_abook_waitable_for_each(OssoABookWaitable *waitable, gboolean call_closure,
   }
 }
 
+/**
+ * osso_abook_waitable_reset:
+ * @waitable : a #OssoABookWaitable
+ *
+ * Resets @waitable by removing all registered callbacks without executing them.
+ **/
 void
 osso_abook_waitable_reset(OssoABookWaitable *waitable)
 {
@@ -161,6 +235,23 @@ waitable_notify_cb(gpointer user_data)
   return FALSE;
 }
 
+/**
+ * osso_abook_waitable_notify:
+ * @waitable: a #OssoABookWaitable
+ * @error: a parameter for indicating an error
+ *
+ * Derived classes will call this function in order to indicate that they have
+ * transitioned to 'ready'. This function should never be called from outside
+ * the implementation of a derived class (to protect agains this, this function
+ * will call osso_abook_waitable_is_ready() and if it returns %FALSE, waitable
+ * will not be considered ready and the registered callbacks will not be
+ * executed).
+ *
+ * This function will return before the registered callbacks are executed. The
+ * callbacks will be executed in an idle handler, and the @error parameter will
+ * be passed to each callback.
+ *
+ */
 void
 osso_abook_waitable_notify(OssoABookWaitable *waitable, const GError *error)
 {
@@ -197,6 +288,15 @@ ready_cb(OssoABookWaitable *waitable, const GError *error, gpointer data)
   g_main_loop_quit(closure->loop);
 }
 
+/**
+ * osso_abook_waitable_run:
+ * @waitable: a #OssoABookWaitable
+ * @context: a #GMainContext to run in
+ * @error: return location for an error or %NULL on success
+ *
+ * This function simply spins a main loop in @context until @waitable has become
+ * ready.
+ */
 void
 osso_abook_waitable_run(OssoABookWaitable *waitable, GMainContext *context,
                         GError **error)
